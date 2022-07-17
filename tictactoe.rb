57f2a9535c8ce272016f136defc0bd549bbe7936
Chris83 <83odie@gmail.com>
2022-07-17 13:27:51 -0400

    Implemented Square class that will make up the game board

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,33 +1,67 @@
+# frozen_string_literal: true
+
+# Class that handles getting input from players
 class Player
   attr_reader :game_piece
 
   def initialize(game_piece)
     @game_piece = game_piece
   end
+
+  def take_square(erorr = '')
+    puts erorr unless erorr == ''
+
+    puts 'Please choose a square(0-8): '
+    square_choice = gets.chomp.to_i
+
+    # Try again if the player input is invalid
+    square_choice = take_square("#{square_choice} is not a value 0-8") if square_choice.negative? || square_choice > 8
+
+    square_choice
+  end
 end
 
+# Class that handles idividual game board squares
 class Square
+  attr_reader :value
+
   def initialize
     @value = ' '
   end
 
-  def is_taken?
-    return @value != ' '
+  def taken?
+    @value != ' '
   end
 
   def take(value)
-    @value = value
+    @value = value unless taken?
+  end
+
+  def reset
+    @value = ' '
   end
 end
 
+# Class that manages the suares that make up the game board
+class GameBoard
+  def initialize
+    @board = Array.new(9, Square)
+  end
+
+  private
+
+  def reset_board
+    @board.each(&:reset)
+  end
+end
+
+# Class that handles input from players and checking for a winner
 class Game
   def initialize
     # Create the two players
     @player_x = Player.new('X')
     @player_o = Player.new('O')
 
-    # Build the game board with 9 Squares
-    @board = Array.new()
-    9.times { @board.push(Square.new()) }
+    @game_board = GameBoard.new
   end
 end
